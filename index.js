9833a94e28cf98a7dd05007b69bf3fb4dd577a1d
wuweiweiwu <wuxx1045@umn.edu>
2018-02-02 15:08:52 -0800

    adding node env

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
-'use strict';
+#!/usr/bin/env node
+"use strict";
 
-var chalk = require('chalk');
+var chalk = require("chalk");
 
 var currentNodeVersion = process.versions.node;
-var semver = currentNodeVersion.split('.');
+var semver = currentNodeVersion.split(".");
 var major = semver[0];
 
 if (major < 4) {
   console.error(
     chalk.red(
-      'You are running Node ' +
+      "You are running Node " +
         currentNodeVersion +
-        '.\n' +
-        'Create React App requires Node 4 or higher. \n' +
-        'Please update your version of Node.'
+        ".\n" +
+        "Create React App requires Node 4 or higher. \n" +
+        "Please update your version of Node."
     )
   );
   process.exit(1);
 }
 
-require('./createReactComponent');
+require("./createReactComponent");
